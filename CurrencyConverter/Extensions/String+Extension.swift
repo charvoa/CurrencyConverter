8dc2fd941a9bfb2e1e0dd200148bbc0e60d9465e
Nicolas <nicolas.charvoz@epitech.eu>
2018-12-18 15:43:29 +0100

    Master commit

--- CurrencyConverter/Extensions/String+Extension.swift
+++ CurrencyConverter/Extensions/String+Extension.swift
@@ -23,4 +23,15 @@ extension String {
         }
         return nil
     }
+
+    func convertToDictionary() -> [String: Any]? {
+        if let data = self.data(using: .utf8) {
+            do {
+                return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        return nil
+    }
 }
--- CurrencyConverterTests/CurrencyConverterTests.swift
+++ CurrencyConverterTests/CurrencyConverterTests.swift
@@ -10,16 +10,6 @@ import XCTest
 @testable import CurrencyConverter
 
 class CurrencyConverterTests: XCTestCase {
-//
-//    let json =
-//    """
-//    {"base":"EUR","date":"2018-09-06","rates":{"AUD":1.6125,"BGN":1.9511,
-//"BRL":4.7803,"CAD":1.5301,"CHF":1.1248,"CNY":7.9261,"CZK":25.653,"DKK":7.4388,
-//"GBP":0.89609,"HKD":9.1105,"HRK":7.4163,"HUF":325.71,"IDR":17282.0,"ILS":4.1606,
-//"INR":83.517,"ISK":127.49,"JPY":129.24,"KRW":1301.6,"MXN":22.312,"MYR":4.8005,
-//"NOK":9.7526,"NZD":1.7591,"PHP":62.442,"PLN":4.308,"RON":4.6274,"RUB":79.384,
-//"SEK":10.565,"SGD":1.5962,"THB":38.039,"TRY":7.6099,"USD":1.1606,"ZAR":17.781}}
-//"""
 
     func testCurrencyViewModel() {
         let currencyModel = CurrencyModel(title: "EUR", baseValue: 1.0)
@@ -30,17 +20,23 @@ class CurrencyConverterTests: XCTestCase {
         XCTAssertEqual(currencyViewModel.currentValue, nil)
     }
 
-    func testCurrencyConverter() {
+    func testConverterService() {
         let gbpModel = CurrencyModel(title: "GBP", baseValue: 1.11)
         let converterService = ConverterService()
         converterService.base = "GBP"
 
-        let result = converterService.convert(3, gbpModel.baseValue)
-
-        XCTAssertEqual(result, 3*1.11)
+        XCTAssertEqual(converterService.convert(3, gbpModel.baseValue), 3*1.11)
+        XCTAssertEqual(converterService.convert(4, gbpModel.baseValue), 4*1.11)
+        XCTAssertEqual(converterService.convert(5.12, gbpModel.baseValue), 5.12*1.11)
+        XCTAssertEqual(converterService.convert(8.45, gbpModel.baseValue), 8.45*1.11)
     }
 
-//    func testJSONResult() {
-//        let data = json.data(using: .utf8)
-//    }
+    func testAPIClient() {
+        let mockClient = APIClient_mock()
+
+        mockClient.get(URL(string: "/mock")!) { (dictionary, error) in
+            XCTAssertNotNil(dictionary?["rates"])
+            XCTAssertNil(error)
+        }
+    }
 }
